daa243343d7a676452525f2a4303201fd1ca948c
GuI3Le <60442125+GuI3Le@users.noreply.github.com>
2025-01-18 18:37:44 +0100

    lab 12 init

--- lab12/lab12r.c
+++ lab12/lab12r.c
@@ -7,10 +7,21 @@ int main(int argc, char *argv[]){
 	if(argc>1){
 		key_t key = atoi(argv[1]);
 		const char *filename = argv[2];
-		FILE *file = fopen(filename, "wb");
+		int fd = open(filename, O_READLY);
 		int shmid = shmget(key,101, 0666);
 		char *shared_memory = (char *)shmat(shmid,NULL,0);
-		shared_memory[0]=1;
+		shared_memory[100]=101;
+		while(true){
+			if(shared_memory[100]==255) break;
+			while(shared_memory[100]!=102){}
+		}
+		printf("copying finished\n");
+		close(fd);
+		shmdt(shared_memory);
+		printf("shared memory detached\n");
+
+		
+
 	}
 	return 0;
 }
--- lab12/lab12s.c
+++ lab12/lab12s.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]){
 	if(argc>0){
-	//key_t key;
-	const char* filename = argv[1];
-	char *shared_memory;
-	//printf("%s\n",file);
-	int id = 'S';
-	key_t key = ftok(filename,id);
-	int shmid = shmget(key, 101, 0666 | IPC_CREAT);
-	shared_memory  = (char *)shmat(shmid,NULL,0);
+		//key_t key;
+		const char* filename = argv[1];
+		char *shared_memory;
+		//printf("%s\n",file);
+		int id = 'S';
+		key_t key = ftok(filename,id);
+		int shmid = shmget(key, 101, 0666 | IPC_CREAT);
+		shared_memory  = (char *)shmat(shmid,NULL,0);
+		//printf("%c\n",shared_memory[200]);
+		printf("shared memory attached, key=%ld\n",key);
+		int fd=open(filename, O_RDONLY);
+		if(fd < 0){
+			perror("Cannot open file");
+			return -1;
+		}
+		printf("waiting for reciver...\n");
+		
 
-	printf("shared memory attached, key=%ld\n",key);
-	FILE *file=fopen(filename, "rb");
-	if(file = NULL){
-		perror("Cannot open file");
-		return -1;
-	}
-	printf("waiting for reciver...\n");
-	while(shared_memory[0] == 0){
-	}
-	printf("ready to write\n");
+		//ssize_t bytes_read = read(fd, shared_memory,100);
+		//printf("%d byte(s) sent..\n", bytes_read);
+		while(shared_memory[100]!=101){
+		}
+		ssize_t bytes_read=100;
+		while(bytes_read==100){
+			bytes_read = read(fd, shared_memory,100);
+			printf("%d byte(s) sent..\n", bytes_read);
+			while(shared_memory[100]!=103){}
+		}
+		
+		close(fd);
+		shmdt(shared_memory);
+		printf("shared memory detached\n");
+		shmctl(shmid, IPC_RMID, NULL);
+		//printf("ready to write\n");
 	}
 	return 0;
 }
